1dd0270609b2fb4ff00b1c7f600849ed4ca90b8c
Joe-Yang1 <348993072@tdsb.ca>
2026-02-18 16:37:33 +0000

    Done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,11 @@
+/**
+	* File: Lesson 1.5: Characters
+	* Author: Joe Yang
+	* Date Created: February 10, 2023
+	* Date Last Modified: February 18, 2023
+	*/
+
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -10,14 +18,32 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		char c = 'a';
+		System.out.println(c);
+	
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+	Scanner input = new Scanner (System.in);
+	System.out.print("Input a word: ");
+	String word;
+	char w;
+	word = input.nextLine();
+	w = word.charAt(2);
+	System.out.println(w);
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+	Scanner input = new Scanner(System.in);
+	System.out.print("Input a word: ");
+	String word;
+	word = input.nextLine();
+	char c;
+	c = word.charAt(1);
+	System.out.println("The second character the user entered: " + c);
+	
 	}
 
 	public static void q4() {
